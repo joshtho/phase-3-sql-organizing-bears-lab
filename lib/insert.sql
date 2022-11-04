304097ffd0f0a9b2b19a84415a8d77c3bce6dabf
Joshua Keane Thompson <joshua.thompson0010@gmail.com>
2022-11-04 11:19:54 -0500

    commit

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr. Chocolate', 3, 'M', 'brown', 'cuddly', true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Rowdy', 2, 'M', 'brown', 'rowdy', true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Tabitha', 4, 'F', 'brown', 'cuddly', true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Sergeant Brown', 5, 'M', 'brown', 'cuddly', true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Melissa', 4, 'F', 'brown', 'cuddly', true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Grinch', 9, 'M', 'brown', 'cuddly', true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Wendy', 12, 'F', 'brown', 'cuddly', false);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL , 3, 'M', 'brown', 'cuddly', true);
